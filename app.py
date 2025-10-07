6f003b6060f0417fa9c24b88532d330939b7af3c
Kartika Musle <kartikamusle@gmail.com>
2025-10-07 19:56:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ st.markdown(
         background-color: #fff !important;
         color: #646f58 !important;
         border: 1px solid #646f58;
+        
+    h1, h2, h3, h4, h5, h6, .stMarkdown p, .stText {
+        color: #000000 !important;
+    }
     }
     </style>
     """,
