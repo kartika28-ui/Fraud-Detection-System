22cc9a02b5cbb475d2f7a6b14a974776fd8a72d5
Kartika Musle <kartikamusle@gmail.com>
2025-10-07 20:10:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown(
         border: 1px solid #646f58;
         
     h1, h2, h3, h4, h5, h6, .stMarkdown p, .stText {
-        color: #000000 !important;
+        color: #646f58 !important;
     }
     }
     </style>
