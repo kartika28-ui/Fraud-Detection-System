0e1fb5655f208be3fc60abda2f4b114e09bee5ad
Kartika Musle <kartikamusle@gmail.com>
2025-10-07 17:14:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,16 +34,15 @@ col1, col2 = st.columns(2)
 with col1:
     type_input = st.selectbox(
         "Transaction Type",
-        ["CASH-IN", "CASH-OUT", "DEBIT", "PAYMENT", "TRANSFER"]
+        ["SELECT", "CASH-IN", "CASH-OUT", "DEBIT", "PAYMENT", "TRANSFER"]
     )
     amount = st.number_input("Transaction Amount", min_value=0.0, step=100.0)
-    oldbalanceOrg = st.number_input("Old Balance (Origin)", min_value=0.0, step=100.0)
-    newbalanceOrig = st.number_input("New Balance (Origin)", min_value=0.0, step=100.0)
+    oldbalanceOrg = st.number_input("Sender's Old Balance", min_value=0.0, step=100.0)
+    newbalanceOrig = st.number_input("Sender's New Balance", min_value=0.0, step=100.0)
 
 with col2:
-    oldbalanceDest = st.number_input("Old Balance (Destination)", min_value=0.0, step=100.0)
-    newbalanceDest = st.number_input("New Balance (Destination)", min_value=0.0, step=100.0)
-    dest_is_customer = st.selectbox("Is Destination a Customer?", ["Yes", "No"])
+    oldbalanceDest = st.number_input("Receiver's Old Balance", min_value=0.0, step=100.0)
+    newbalanceDest = st.number_input("Receiver's New Balance", min_value=0.0, step=100.0)
 
 OrigBalanceChange = oldbalanceOrg - newbalanceOrig - amount
 DestBalanceChange = newbalanceDest - oldbalanceDest - amount
